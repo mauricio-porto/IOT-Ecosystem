45ae1b68bcb2bf42e662b4572137f78b31ce2cd6
mauricioschramm <mauricio.schramm@hp.com>
2014-01-10 18:56:28 -0200

    Initial Proto for converse updating gauge

---
+++ OBDremote/AutoDiagProto/js/initGauges.js
@@ -0,0 +1,26 @@
+﻿var aGauge = null;
+var sGauge = null;
+var wGauge = null;
+
+function initGauges() {
+    aGauge = new AquaGauge('gauge');
+    aGauge.props.minValue = 0;
+    aGauge.props.maxValue = 100;
+    aGauge.props.noOfDivisions = 5;
+    aGauge.props.noOfSubDivisions = 4;
+    aGauge.props.showMinorScaleValue = true;
+    aGauge.refresh(0);
+    initPage();
+    setupSlider();
+    setupRangeSliders();
+
+    sGauge = new AquaGauge('gaugeSmall');
+    setupSmallGauge(sGauge);
+
+    wGauge = new AquaGauge('gaugeWhite');
+    setupWhiteGauge(wGauge);
+}
+
+function updateGauge(val) {
+    aGauge.refresh(val);
+}
